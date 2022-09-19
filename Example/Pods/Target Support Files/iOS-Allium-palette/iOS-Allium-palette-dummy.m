56273003ef7b45b3093099dbf3d12a48caa341e9
QuickChangeSpidey <akshay.pandey@telus.com>
2022-09-19 09:07:34 -0400

    initial files added

---
+++ Example/Pods/Target Support Files/iOS-Allium-palette/iOS-Allium-palette-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_iOS_Allium_palette : NSObject
+@end
+@implementation PodsDummy_iOS_Allium_palette
+@end
---
+++ iOS-Allium-palette/Classes/ios/Color.swift
@@ -0,0 +1,53 @@
+
+//
+// Color.swift
+//
+
+// Do not edit directly
+// Generated on Wed, 31 Aug 2022 15:19:17 GMT
+
+
+import UIKit
+
+public enum PaletteColor {
+    public static let amber = UIColor(red: 0.980, green: 0.792, blue: 0.412, alpha: 1)
+    public static let amberDark = UIColor(red: 0.706, green: 0.529, blue: 0.173, alpha: 1)
+    public static let amberLight = UIColor(red: 1.000, green: 0.976, blue: 0.933, alpha: 1)
+    public static let bgModal = UIColor(red: 0.957, green: 0.957, blue: 0.969, alpha: 0.97)
+    public static let dark10 = UIColor(red: 0.000, green: 0.000, blue: 0.000, alpha: 0.1)
+    public static let dark20 = UIColor(red: 0.000, green: 0.000, blue: 0.000, alpha: 0.2)
+    public static let dark40 = UIColor(red: 0.000, green: 0.000, blue: 0.000, alpha: 0.4)
+    public static let dark60 = UIColor(red: 0.000, green: 0.000, blue: 0.000, alpha: 0.6)
+    public static let dark80 = UIColor(red: 0.000, green: 0.000, blue: 0.000, alpha: 0.8)
+    public static let dark97 = UIColor(red: 0.000, green: 0.000, blue: 0.000, alpha: 0.97)
+    public static let greenAccessible = UIColor(red: 0.169, green: 0.502, blue: 0.000, alpha: 1)
+    public static let greenDarkFern = UIColor(red: 0.086, green: 0.243, blue: 0.024, alpha: 1)
+    public static let greenPanache = UIColor(red: 0.957, green: 0.976, blue: 0.949, alpha: 1)
+    public static let greenSanFelix = UIColor(red: 0.122, green: 0.361, blue: 0.035, alpha: 1)
+    public static let greenTelus = UIColor(red: 0.400, green: 0.800, blue: 0.000, alpha: 1)
+    public static let greenTusk = UIColor(red: 0.890, green: 0.965, blue: 0.820, alpha: 1)
+    public static let greenYellow = UIColor(red: 0.749, green: 0.906, blue: 0.592, alpha: 1)
+    public static let greyAlabaster = UIColor(red: 0.980, green: 0.980, blue: 0.980, alpha: 1)
+    public static let greyAthens = UIColor(red: 0.957, green: 0.957, blue: 0.969, alpha: 1)
+    public static let greyCharcoal = UIColor(red: 0.255, green: 0.271, blue: 0.278, alpha: 1)
+    public static let greyCloud = UIColor(red: 0.698, green: 0.725, blue: 0.749, alpha: 1)
+    public static let greyMystic = UIColor(red: 0.890, green: 0.902, blue: 0.910, alpha: 1)
+    public static let greyShuttle = UIColor(red: 0.404, green: 0.431, blue: 0.451, alpha: 1)
+    public static let greyThunder = UIColor(red: 0.173, green: 0.180, blue: 0.188, alpha: 1)
+    public static let light10 = UIColor(red: 1.000, green: 1.000, blue: 1.000, alpha: 0.1)
+    public static let light20 = UIColor(red: 1.000, green: 1.000, blue: 1.000, alpha: 0.2)
+    public static let light40 = UIColor(red: 1.000, green: 1.000, blue: 1.000, alpha: 0.4)
+    public static let light60 = UIColor(red: 1.000, green: 1.000, blue: 1.000, alpha: 0.6)
+    public static let light80 = UIColor(red: 1.000, green: 1.000, blue: 1.000, alpha: 0.8)
+    public static let light97 = UIColor(red: 1.000, green: 1.000, blue: 1.000, alpha: 0.97)
+    public static let purpleDark = UIColor(red: 0.247, green: 0.165, blue: 0.329, alpha: 1)
+    public static let purpleDeluge = UIColor(red: 0.486, green: 0.325, blue: 0.647, alpha: 1)
+    public static let purpleEminence = UIColor(red: 0.380, green: 0.220, blue: 0.537, alpha: 1)
+    public static let purpleTelus = UIColor(red: 0.294, green: 0.157, blue: 0.427, alpha: 1)
+    public static let red = UIColor(red: 0.882, green: 0.137, blue: 0.224, alpha: 1)
+    public static let redDark = UIColor(red: 0.757, green: 0.137, blue: 0.208, alpha: 1)
+    public static let redDarker = UIColor(red: 0.337, green: 0.047, blue: 0.047, alpha: 1)
+    public static let redLight = UIColor(red: 1.000, green: 0.965, blue: 0.973, alpha: 1)
+    public static let transparent = UIColor(red: 0.000, green: 0.000, blue: 0.000, alpha: 0)
+    public static let white = UIColor(red: 1.000, green: 1.000, blue: 1.000, alpha: 1)
+}
