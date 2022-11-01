b1cfe90de8871cf4b24cefe81e3a92065f6e533a
QuickChangeSpidey <akshay.pandey@telus.com>
2022-10-31 23:38:18 -0400

    major changes

--- auto.py
+++ auto.py
@@ -29,7 +29,7 @@ def podCommandEdit():
     global pod_push_command
     source_suffix = 'https://github.com/CocoaPods/Specs.git --allow-warnings'
     lib_command = 'pod lib lint --sources='
-    pod_push_command = 'pod repo push ' + project_name + ' ' + podspec_file_name
+    pod_push_command = 'pod trunk push'
     if len(sources) > 0:
         # rely on  private sourece
         pod_push_command += ' --sources='
